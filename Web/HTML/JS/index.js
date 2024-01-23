dec288078015cbf7421bc0622ead1b81452e0eb6
Matheus Oliveira <72510366+Neural-Matheus@users.noreply.github.com>
2024-01-23 00:37:07 -0300

    ✨ feat: Início das páginas
    Início da criação da página de registro e da página de login.

---
+++ Web/HTML/JS/index.js
@@ -0,0 +1,8 @@
+document.getElementById('togglePassword').addEventListener('click', function () {
+    var passwordInput = document.getElementById('password');
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+});
