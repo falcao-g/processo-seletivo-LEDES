4c8c9bf9b8da369266831fc2348b40011a884f3e
Wagner Rodrigues <127705012+wagnerUFMS@users.noreply.github.com>
2024-01-23 17:00:48 -0400

    define porta localhost (#11)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
