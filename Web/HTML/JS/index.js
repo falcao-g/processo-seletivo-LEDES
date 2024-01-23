93d5fbb9ad463b1cb2dfa8cd0afd52f9fa845b82
Matheus Oliveira <vinicius.matheus@ufms.br>
2024-01-23 11:42:40 -0300

    :fire: Remoção da pasta Web

--- Web/HTML/JS/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('togglePassword').addEventListener('click', function () {
-    var passwordInput = document.getElementById('password');
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-    } else {
-        passwordInput.type = "password";
-    }
-});
