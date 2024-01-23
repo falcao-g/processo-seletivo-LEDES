2b64f815016a4dece354aecb3ba551aa312da9e1
Falcão <gabriel.p.falcao50@gmail.com>
2024-01-23 14:39:02 -0400

    feat: cria as migrations

---
+++ src/migrations/20240123182049_cria_tabela_usuario.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => knex.schema.createTable('usuario', (table) => {
+  table.uuid('uuid').primary().notNullable();
+  table.string('nome', 100).notNullable();
+  table.string('cargo', 100);
+  table.string('cpf', 11).unique().notNullable();
+  table.string('password', 100).notNullable();
+  table.binary('foto');
+  table.date('dataNascimento');
+  table.increments('matricula');
+  table.enum('situacaoCracha', ['ANALISE', 'APROVADO', 'RECUSADO']).defaultTo('ANALISE');
+  table.enum('papel', ['ADMIN', 'USER']).defaultTo('USER');
+});
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => knex.schema.dropTableIfExists('usuario');
