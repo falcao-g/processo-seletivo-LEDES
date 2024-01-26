634755b1e82771506ae7e19d1d178ece7364482c
Falcão <gabriel.p.falcao50@gmail.com>
2024-01-25 20:21:43 -0400

    fix: conserta problemas de merge

---
+++ src/api.js
@@ -0,0 +1,18 @@
+const api = require('express')();
+const express = require('express');
+const path = require('path');
+const authRouter = require('./auth/auth.route');
+// eslint-disable-next-line import/no-unresolved
+const userRouter = require('./user/user.route');
+const adminRouter = require('./admin/admin.route');
+require('dotenv').config();
+
+api.use(express.json());
+
+api.use(express.static(path.join(__dirname, '../public')));
+
+api.use('/auth', authRouter);
+api.use('/user', userRouter);
+api.use('/admin', adminRouter);
+
+module.exports = { api };
